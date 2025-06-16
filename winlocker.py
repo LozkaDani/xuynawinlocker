29a997ccdbcf82e2d4a01a4aa0eaef44ff8c3871
Alex9600t <dukovaleks2@gmail.com>
2025-06-16 19:06:05 +0300

    fix

--- winlocker.py
+++ winlocker.py
@@ -1,17 +1,21 @@
 import tkinter as tk
+import asyncio
 
-win = tk.Tk()
-
-win.title("Не винлокер")
-win.geometry("1920x1080")
-
-tk.Label(win, text="я питарас").pack()
-
-win.mainloop()
-for i in range(999):
+async def openWindow(i):
    new_win = tk.Toplevel()
    new_win.title(f"Окно-пидарас Номер {i+1}")
    new_win.geometry("1920x1080")
-
    tk.Label(new_win, text="И я тоже питарас!").pack()
-   new_win.mainloop()
+
+def main():
+    win = tk.Tk()
+    win.title("Не винлокер")
+    win.geometry("1920x1080")
+    tk.Label(win, text="я питарас").pack()
+
+    for i in range(69):
+        asyncio.run(openWindow(i))
+    win.mainloop()
+
+if (__name__ == "__main__"):
+    main()
