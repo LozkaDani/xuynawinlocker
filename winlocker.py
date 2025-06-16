194aa7583e06f777eb50acdc2cb66b574524271d
Alex9600t <dukovaleks2@gmail.com>
2025-06-16 18:01:43 +0300

    fix(Real winlocker)

--- winlocker.py
+++ winlocker.py
@@ -7,6 +7,7 @@ win.geometry("1920x1080")
 
 tk.Label(win, text="я питарас").pack()
 
+win.mainloop()
 for i in range(999):
    new_win = tk.Toplevel()
    new_win.title(f"Окно-пидарас Номер {i+1}")
@@ -14,5 +15,3 @@ for i in range(999):
 
    tk.Label(new_win, text="И я тоже питарас!").pack()
    new_win.mainloop()
-
-win.mainloop()
